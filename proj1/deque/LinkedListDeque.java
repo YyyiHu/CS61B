a13aa69b159f405dd3354edb05405bf63bdca28c
RavenHuY <maxhu00@163.com>
2023-05-25 23:49:01 +0800

    Completed Linked List Deque

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -32,6 +32,12 @@ public class LinkedListDeque<T> {
     }
 
     public void addLast(T item) {
+        Node newNode = new Node<>(null, item, null);
+        sentinel.prev.next = newNode;
+        newNode.prev = sentinel.prev;
+        newNode.next = sentinel;
+        sentinel.prev = newNode;
+        size++;
     }
 
     // Return true id deque is empty, false otherwise
@@ -48,9 +54,62 @@ public class LinkedListDeque<T> {
 
     // Prints the items in the deque from first to last, separated by a space
     public void printDeque() {
-
+        Node printNode = sentinel.next;
+        while (printNode != sentinel) {
+            System.out.print(printNode.item + " ");
+            printNode = printNode.next;
+        }
+        System.out.println();
     }
 
+    // Removes and returns the item at the front of the deque
+    // If no such item exists, returns null
+    public T removeFirst() {
+        if (sentinel.next == sentinel) {
+            return null;
+        }
+        T first = (T) sentinel.next.item;
+        sentinel.next.next.prev = sentinel;
+        sentinel.next = sentinel.next.next;
+        size--;
+        return first;
+    }
+
+    public T removeLast() {
+        if (sentinel.next == sentinel) {
+            return null;
+        }
+        T last = (T) sentinel.prev.item;
+        sentinel.prev.prev.next = sentinel;
+        sentinel.prev = sentinel.prev.prev;
+        size--;
+        return last;
+    }
+
+    // Gets the item at the given index, where 0 is the front
+    // If no such item exists, returns null
+    public T get(int index) {
+        if (sentinel.next == sentinel) {
+            return null;
+        }
+        Node node = sentinel;
+        for(int i = 0; i <= index; i++) {
+            node = node.next;
+        }
+        return (T) node.item;
+    }
+
+    public T getRecursive(int index) {
+        LinkedListDeque deque = new LinkedListDeque();
+        deque.sentinel = this.sentinel.next;
+        if (this.sentinel.next == this.sentinel) {
+            return null;
+        } else if (index == 0) {
+            return (T) deque.sentinel.item;
+        } else {
+            return (T) deque.getRecursive(index - 1);
+        }
+    }
 
     // Constructor for an empty linked list deque
     public LinkedListDeque() {
@@ -59,13 +118,21 @@ public class LinkedListDeque<T> {
         size = 0;
     }
 
-    //public T getRecursive(int index) {
+
 
     public static void main(String[] args) {
         LinkedListDeque deque = new LinkedListDeque();
-        deque.addFirst(1);
-        deque.addFirst(2);
         deque.addFirst(3);
+        deque.addFirst(2);
+        deque.addFirst(1);
+        deque.addLast(4);
+        deque.addLast(5);
+        deque.printDeque();
+        deque.removeFirst();
+        deque.removeLast();
+        deque.printDeque();
+        System.out.println(deque.get(1));
+        System.out.println(deque.getRecursive(1));
     }
 
 }
