82cff1aead1413f9a9a800d79647d9c44ab1021f
Yi <maxhu00@163.com>
2023-08-21 23:42:51 +0200

    For lab4, commit first version of Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,7 +1,17 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author Yi
  */
 public class Collatz {
+
+    /**
+     * Calculates the next number in the Collatz sequence based on the given number.
+     *
+     * If the input number is odd, the next number is calculated as 3 * n + 1.
+     * If the input number is even, the next number is calculated as n / 2.
+     *
+     * @param n The current number in the Collatz sequence.
+     * @return The next number in the Collatz sequence.
+     */
     public static int nextNumber(int n) {
         if (n % 2 == 1) {
             return 3 * n + 1;
@@ -12,11 +22,10 @@ public class Collatz {
 
     public static void main(String[] args) {
         int n = 5;
-        System.out.print(n + " ");
         while (n != 1) {
-            n = nextNumber(n);
             System.out.print(n + " ");
+            n = nextNumber(n);
         }
+        System.out.println(n);
     }
 }
-
